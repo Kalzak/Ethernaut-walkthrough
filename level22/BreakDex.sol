5c3c67c72d0215e1516b76d70ccd1987c921e3d1
Kalzak <kalzakdev@protonmail.com>
2021-08-27 23:36:25 +0800

    new level

---
+++ level22/BreakDex.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+contract BreakDex {
+    function balanceOf(address caller) public view returns (uint256) {
+        return 1;
+    }
+    
+    function transferFrom(address from, address to, uint256 amount) public returns(bool){
+        return true;
+    }
+}
